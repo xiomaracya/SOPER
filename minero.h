55074f08fefe8dd1628c9a548b3405df15dac70f
xiomaracya <lizbethyxiomara@gmail.com>
2025-02-26 01:42:37 +0100

    documentacion

--- minero.h
+++ minero.h
@@ -1,9 +1,31 @@
+/**
+ * @file minero.h
+ * @author Sara Serrano Marazuela
+ * @author Xiomara Caballero Cuya
+ * @brief Module minero
+ * @version 2.0
+ * @date 2024-02-01
+ *
+ */
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+/**
+ * @brief Minates a function with a number of thread and rounds
+ *
+ * @param rondas number of rounds to search a target
+ * @param hilos number of thread to use
+ * @param objetivo solution to be searched
+ * @return status of the operation EXIT_FAILURE or EXIT_SUCCESS
+ */
 int proceso_minero(int rondas, int hilos, long int objetivo, int fd_escritura, int fd_lectura);
 
+/**
+ * @brief search in the function pow_hash a target in a ragne of number
+ *
+ * @param arg dates to search in Minero
+ */
 void *busqueda(void *arg);
--- monitor.h
+++ monitor.h
@@ -1,7 +1,22 @@
+/**
+ * @file monitor.h
+ * @author Sara Serrano Marazuela
+ * @author Xiomara Caballero Cuya
+ * @brief Module monitor
+ * @version 2.0
+ * @date 2024-02-01
+ *
+ */
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+/**
+ * @brief Verify the solution found by Minero
+ *
+ * @param fd_escritura file descriptor that allows to write in the second pipe
+ * @param fd_lectura file descriptor that allows to read in the first pipe
+ */
 int monitor(int fd_escritura, int fd_lectura);
